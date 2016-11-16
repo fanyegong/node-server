35c55ef6b54bf7070f093abc0554ce6e0bc782a7
fanyegong <fengliyuar@163.com>
2016-11-16 16:31:57 +0800

    modify app.js about routes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// use routes
+var indexRoute = require('./routes/index.js');
+var userRoute = require('./routes/user.js');
+app.use('/', indexRoute); 
+app.use('/user', userRoute);
+
+//app.get('/', function (req, res) {
+//    res.send('Hello World!');
+//});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
