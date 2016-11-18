fb30a90e4b57a048caea257a523f02b1dc1daa7d
fanyegong <fengliyuar@163.com>
2016-11-18 20:51:37 +0800

    use https protocol

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey = fs.readFileSync('sslcert/2_www.cpcwe.com.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/1_www.cpcwe.com_bundle.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
 var express = require('express');
 var app = express();
 
@@ -14,7 +21,11 @@ app.use('/user', userRoute);
 app.use(express.static('public'));
 
 // Starting server
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+//app.listen(3000, function () {
+//    console.log('Example app listening on port 3000!');
+//});
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpServer.listen(80);
+httpsServer.listen(443);
 
